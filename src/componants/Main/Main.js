9b726b5fb6a80b58f93cc6fd7aab3d3ea11c2596
Vikas <vikas@Vikass-MacBook-Air.local>
2023-01-14 09:33:53 +0530

    additional feature

--- src/componants/Main/Main.js
+++ src/componants/Main/Main.js
@@ -1,19 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./main.scss";
 import { restaurantList } from "../../common/utils/dummayData";
 import Card from "./Card";
 
 const filterData = (searchWith, list) => {
-  let aa = list.filter((res) =>
+  return list.filter((res) =>
     (res.data.data.name + res.data.data.cuisines.join(" "))
       .toLowerCase()
       .includes(searchWith.toLowerCase())
   );
-  return aa;
 };
+
+const filterByFood = () => {
+  let foodTypes = [];
+  restaurantList.map((res) => {
+    foodTypes.push(...res.data.data.cuisines);
+  });
+  return [...new Set(foodTypes)];
+};
+
 const Main = () => {
   const [restaurants, setRestaurants] = useState(restaurantList);
   const [searchText, setSearchText] = useState("");
+  const [toggle, seToggle] = useState(false);
+  const [checkedItems, setCheckedItems] = useState({});
+  const [vegType, setVegType] = useState(false);
+  const textInput = useRef(null);
+
+  const Switch = () => {
+    return (
+      <>
+        <input
+          className="react-switch-checkbox"
+          id={`react-switch-new`}
+          type="checkbox"
+          onChange={(e) => filterVag(e)}
+        />
+        <label className="react-switch-label" htmlFor={`react-switch-new`}>
+          <span className={`react-switch-button`} />
+        </label>
+      </>
+    );
+  };
+  function filterVag(e) {
+    console.log(e.target.checked);
+    setVegType(e.target.checked);
+    let vegList;
+    if (e.target.checked) {
+      vegList = restaurantList.filter((res) => !res.data.data?.veg);
+    } else {
+      vegList = restaurantList.filter((res) => res.data.data.veg);
+    }
+    setRestaurants(vegList);
+  }
+  function handleChange(e) {
+    setCheckedItems({
+      ...checkedItems,
+      [e.target.name]: e.target.checked,
+    });
+  }
+  function toggleFilter() {
+    seToggle(!toggle);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const selectedItems = Object.keys(checkedItems).filter(
+      (item) => checkedItems[item]
+    );
+    let filteredArray = restaurantList.filter((item) => {
+      if (selectedItems.length) {
+        for (i = 0; i < item.data.data.cuisines.length; i++) {
+          if (selectedItems.includes(item.data.data.cuisines[i])) {
+            return true;
+            break;
+          }
+        }
+      } else {
+        return true;
+      }
+    });
+    console.log(filteredArray, selectedItems, restaurants);
+    setRestaurants(filteredArray);
+  }
+
   return (
     <>
       <div className="main">
@@ -40,11 +109,52 @@ const Main = () => {
                 Search
               </button>
             </div>
-            <div className="result-count">
-              {restaurants.length
-                ? `${restaurants.length} Restaurants found`
-                : "No restaurants found"}
+            <div className="filters">
+              <span>{vegType ? "Non-veg" : "Vag"}</span>
+              <input
+                className="react-switch-checkbox"
+                id={`react-switch-new`}
+                type="checkbox"
+                onChange={(e) => filterVag(e)}
+              />
+              <label
+                className="react-switch-label"
+                htmlFor={`react-switch-new`}
+              >
+                <span className={`react-switch-button`} />
+              </label>
+              <button onClick={toggleFilter}>Filter By Food</button>
             </div>
+            <div className="result-count">
+              <span>
+                {restaurants.length
+                  ? `${restaurants.length} Restaurants found`
+                  : "No restaurants found"}
+              </span>
+            </div>
+          </div>
+          <div className="foot-type">
+            {toggle && (
+              <form>
+                {filterByFood().map((res) => {
+                  return (
+                    <div className="input-box" key={res}>
+                      <input
+                        type="checkbox"
+                        name={res}
+                        id={`ab-${res}`}
+                        onChange={handleChange}
+                        ref={textInput}
+                      />
+                      <label htmlFor={`ab-${res}`}>
+                        <span>{res}</span>
+                      </label>
+                    </div>
+                  );
+                })}
+                <button onClick={handleSubmit}>Apply Filter</button>
+              </form>
+            )}
           </div>
           <div className="resturants">
             {restaurants.length ? (
@@ -54,7 +164,7 @@ const Main = () => {
                 );
               })
             ) : (
-              <p>Result not found......</p>
+              <p className="alert">Result not found......</p>
             )}
           </div>
         </div>
